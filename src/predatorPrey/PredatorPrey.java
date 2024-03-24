6449babb76b890847d3db20f25a5fd21ff3d1a6c
Yuvraj Sehgal <17yuvraj.sehgal@gmail.com>
2024-03-23 22:33:30 -0400

    changed food to prey

--- src/predatorPrey/PredatorPrey.java
+++ src/predatorPrey/PredatorPrey.java
@@ -19,14 +19,16 @@ public class PredatorPrey extends GPProblem implements SimpleProblemForm {
     public static final String P_DATA = "data";
     public int MOVES = 0;
     public Predator predator;
+    public Preys preys;
     public int TOTAL_PREY_KILLED = 0;
     public final int BOARD_ROWS = 32;
     public final int BOARD_COLUMNS = 32;
     public final int[][] ground = new int[BOARD_ROWS][BOARD_COLUMNS];
     public int MAX_MOVES;
     public int TOTAL_PREY = 0;
-    public int[] preyX; //initial x coordinates of prey
-    public int[] preyY; //initial y coordinates of prey
+    public int MAX_PREYS = 89;
+//    public int[] preyX; //initial x coordinates of prey
+//    public int[] preyY; //initial y coordinates of prey
 
     @Override
     public void setup(final EvolutionState state, final Parameter base) {
@@ -45,7 +47,8 @@ public class PredatorPrey extends GPProblem implements SimpleProblemForm {
     private void readGrid(final EvolutionState state, final Parameter base) {
         //read the name of the file where we store the trail data from the parameter file under the field data.
         InputStream inputStream = state.parameters.getResource(base.push("file"), null);
-        this.predator = new Predator(new Point(0,0),this.ground,this.BOARD_ROWS,this.BOARD_COLUMNS);        //start reading file
+        this.predator = new Predator(new Point(0, 0), this.ground, this.BOARD_ROWS, this.BOARD_COLUMNS);
+        this.preys = new Preys(MAX_PREYS);
         if (inputStream != null) {
             LineNumberReader lineNumberReader = null;
             try {
@@ -65,17 +68,15 @@ public class PredatorPrey extends GPProblem implements SimpleProblemForm {
                             this.ground[i][j] = 1;
                         else if (data.charAt(j) == '#') {
                             this.ground[i][j] = -1;
+                            preys.addPrey(new Prey(new Point(i, j), ground, BOARD_ROWS, BOARD_COLUMNS));
                             this.TOTAL_PREY++;
                         } else if (data.charAt(j) == '.') {
                             this.ground[i][j] = 2;
                         } else
                             state.output.error("Bad character '" + data.charAt(j) + "' on line number " + lineNumberReader.getLineNumber() + " of the predator trail file.");
                     }
-
                 }
-
-                getPreyLocations();
-
+                //getPreyLocations();
             } catch (IOException e) {   //in case error occurred while reading file
                 state.output.fatal("The predator.trl file could not be read due to an IOException:\n" + e);
             } finally {  //close the input reader if not null
@@ -89,33 +90,32 @@ public class PredatorPrey extends GPProblem implements SimpleProblemForm {
             state.output.exitIfErrors();
 
 
-
         }
         //if there was an issue while reading the predator.trl file, throw error.
         else {
             state.output.fatal("Error encountered while loading file or resource", base.push("file"), null);
         }
-        printArray();
-        System.out.println("x coordinates of preys:"+ Arrays.toString(this.preyX));
-        System.out.println("x coordinates of preys:"+ Arrays.toString(this.preyY));
+//        printArray();
+//        System.out.println("x coordinates of preys:" + Arrays.toString(this.preyX));
+//        System.out.println("x coordinates of preys:" + Arrays.toString(this.preyY));
 
     }
 
-    private void getPreyLocations() {
-        this.preyX = new int[this.TOTAL_PREY];
-        this.preyY = new int[this.TOTAL_PREY];
-        int counter = 0;
-
-        for(int i=0;i<this.ground.length;i++){
-            for (int j=0;j<this.ground[0].length;j++){
-                if (ground[i][j]==-1){
-                    this.preyX[counter]=i;
-                    this.preyY[counter]=j;
-                    counter++;
-                }
-            }
-        }
-    }
+//    private void getPreyLocations() {
+//        this.preyX = new int[this.TOTAL_PREY];
+//        this.preyY = new int[this.TOTAL_PREY];
+//        int counter = 0;
+//
+//        for (int i = 0; i < this.ground.length; i++) {
+//            for (int j = 0; j < this.ground[0].length; j++) {
+//                if (ground[i][j] == -1) {
+//                    this.preyX[counter] = i;
+//                    this.preyY[counter] = j;
+//                    counter++;
+//                }
+//            }
+//        }
+//    }
 
     public void printArray() {
         for (int i = 0; i < this.BOARD_ROWS; i++) {
@@ -124,36 +124,37 @@ public class PredatorPrey extends GPProblem implements SimpleProblemForm {
             }
             System.out.println();
         }
+        System.out.println();
     }
 
     @Override
     public void evaluate(EvolutionState evolutionState, Individual individual, int threadNum, int i1) {
         if (!individual.evaluated) {
-            this.TOTAL_PREY_KILLED=0;
-            this.predator.point.x=0;
-            this.predator.point.y=0;
-            this.predator.orientation=Orientation.RIGHT;
-            this.MOVES=0;
+            this.TOTAL_PREY_KILLED = 0;
+            this.predator.location.x = 0;
+            this.predator.location.y = 0;
+            this.predator.orientation = Orientation.RIGHT;
+            this.MOVES = 0;
         }
 
-        while (this.MAX_MOVES>this.MOVES && this.TOTAL_PREY>this.TOTAL_PREY_KILLED){
-            ((GPIndividual)individual).trees[0].child.eval(evolutionState, threadNum, this.input, this.stack, (GPIndividual)individual, this);
+        while (this.MAX_MOVES > this.MOVES && this.TOTAL_PREY > this.TOTAL_PREY_KILLED) {
+            ((GPIndividual) individual).trees[0].child.eval(evolutionState, threadNum, this.input, this.stack, (GPIndividual) individual, this);
         }
 
         KozaFitness fitness = (KozaFitness) individual.fitness;
-        fitness.setStandardizedFitness(evolutionState,this.TOTAL_PREY-this.TOTAL_PREY_KILLED);
+        fitness.setStandardizedFitness(evolutionState, this.TOTAL_PREY - this.TOTAL_PREY_KILLED);
         fitness.hits = this.TOTAL_PREY_KILLED;
-        individual.evaluated=true;
+        individual.evaluated = true;
 
         //restore the preys to the initial positions
-        restorePreys();
+        preys.restorePreyLocations();
 
 
     }
 
-    private void restorePreys() {
-        for(int i=0;i<TOTAL_PREY;i++){
-            this.ground[this.preyX[i]][this.preyY[i]]=-1;
-        }
-    }
+//    private void restorePreys() {
+//        for (int i = 0; i < TOTAL_PREY; i++) {
+//            this.ground[this.preyX[i]][this.preyY[i]] = -1;
+//        }
+//    }
 }
