536165fa4fc829448819569560a19488a13499e2
Yuvraj Sehgal <17yuvraj.sehgal@gmail.com>
2024-03-24 21:48:26 -0400

    added comments

--- src/functions/Progn2.java
+++ src/functions/Progn2.java
@@ -1,3 +1,6 @@
+/**
+ * Represents a node in a genetic programming tree that executes its two children sequentially.
+ */
 package functions;
 
 import ec.EvolutionState;
@@ -9,22 +12,51 @@ import ec.gp.GPIndividual;
 import ec.gp.GPNode;
 
 public class Progn2 extends GPNode implements EvalPrint {
+
+    /**
+     * Returns a string representation of the node.
+     * @return The string representation of the node.
+     */
     @Override
     public String toString() {
         return "PROGN3";
     }
 
+    /**
+     * Specifies the number of children expected for this node.
+     * @return The expected number of children (2 in this case).
+     */
     @Override
     public int expectedChildren() {
         return 2;
     }
 
+    /**
+     * Evaluates the node's logic by sequentially evaluating its two children.
+     * @param state The current evolution state.
+     * @param thread The thread number.
+     * @param input The GP data.
+     * @param stack The ADF stack.
+     * @param individual The GP individual.
+     * @param problem The problem instance.
+     */
     @Override
     public void eval(EvolutionState state, int thread, GPData input, ADFStack stack, GPIndividual individual, Problem problem) {
         this.children[0].eval(state, thread, input, stack, individual, problem);
         this.children[1].eval(state, thread, input, stack, individual, problem);
     }
 
+    /**
+     * Evaluates the node's logic and prints debug information.
+     * Executes the first child's logic and then the second child's logic.
+     * @param state The current evolution state.
+     * @param thread The thread number.
+     * @param input The GP data.
+     * @param stack The ADF stack.
+     * @param individual The GP individual.
+     * @param problem The problem instance.
+     * @param map2 Two-dimensional array holding integer data.
+     */
     @Override
     public void evalPrint(EvolutionState state, int thread, GPData input, ADFStack stack, GPIndividual individual, Problem problem, int[][] map2) {
         ((EvalPrint)this.children[0]).evalPrint(state, thread, input, stack, individual, problem, map2);
--- src/functions/Progn3.java
+++ src/functions/Progn3.java
@@ -1,3 +1,6 @@
+/**
+ * Represents a node in a genetic programming tree that executes its three children sequentially.
+ */
 package functions;
 
 import ec.EvolutionState;
@@ -9,16 +12,34 @@ import ec.gp.GPIndividual;
 import ec.gp.GPNode;
 
 public class Progn3 extends GPNode implements EvalPrint {
+
+    /**
+     * Returns a string representation of the node.
+     * @return The string representation of the node.
+     */
     @Override
     public String toString() {
         return "PROGN3";
     }
 
+    /**
+     * Specifies the number of children expected for this node.
+     * @return The expected number of children (3 in this case).
+     */
     @Override
     public int expectedChildren() {
         return 3;
     }
 
+    /**
+     * Evaluates the node's logic by sequentially evaluating its three children.
+     * @param state The current evolution state.
+     * @param thread The thread number.
+     * @param input The GP data.
+     * @param stack The ADF stack.
+     * @param individual The GP individual.
+     * @param problem The problem instance.
+     */
     @Override
     public void eval(EvolutionState state, int thread, GPData input, ADFStack stack, GPIndividual individual, Problem problem) {
         this.children[0].eval(state, thread, input, stack, individual, problem);
@@ -26,6 +47,17 @@ public class Progn3 extends GPNode implements EvalPrint {
         this.children[2].eval(state, thread, input, stack, individual, problem);
     }
 
+    /**
+     * Evaluates the node's logic and prints debug information.
+     * Executes the logic of the first child, then the second child, and finally the third child.
+     * @param state The current evolution state.
+     * @param thread The thread number.
+     * @param input The GP data.
+     * @param stack The ADF stack.
+     * @param individual The GP individual.
+     * @param problem The problem instance.
+     * @param map2 Two-dimensional array holding integer data.
+     */
     @Override
     public void evalPrint(EvolutionState state, int thread, GPData input, ADFStack stack, GPIndividual individual, Problem problem, int[][] map2) {
         ((EvalPrint)this.children[0]).evalPrint(state, thread, input, stack, individual, problem, map2);
