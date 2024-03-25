4a3137c9d547e49cff20ebb1ec26aaa27de06281
Yuvraj Sehgal <17yuvraj.sehgal@gmail.com>
2024-03-24 22:22:18 -0400

    added comments

--- src/predatorPrey/Preys.java
+++ src/predatorPrey/Preys.java
@@ -1,3 +1,6 @@
+/**
+ * Represents a collection of preys in a predator-prey simulation.
+ */
 package predatorPrey;
 
 import java.awt.*;
@@ -6,21 +9,33 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Preys {
-    private final List<Prey> preyList;
-    private final List<Point> initialLocations;
-    int[][] ground;
+    private final List<Prey> preyList; // List to store the preys.
+    private final List<Point> initialLocations; // List to store the initial locations of the preys.
+    int[][] ground; // The ground grid.
 
+    /**
+     * Constructor for the Preys class.
+     * @param totalPreys The total number of preys.
+     * @param ground The ground grid.
+     */
     public Preys(int totalPreys, int[][] ground){
         this.preyList = new ArrayList<>(totalPreys);
         this.initialLocations = new ArrayList<>(totalPreys);
         this.ground = ground;
     }
 
+    /**
+     * Adds a prey to the collection.
+     * @param prey The prey to add.
+     */
     public void addPrey(Prey prey){
         this.preyList.add(prey);
         this.initialLocations.add(prey.location);
     }
 
+    /**
+     * Restores the initial locations of all the preys.
+     */
     public void restorePreyLocations() {
         for(int[] row:ground){
             Arrays.fill(row,1);
@@ -29,11 +44,12 @@ public class Preys {
             Prey prey = preyList.get(i);
             Point location = prey.location = initialLocations.get(i);
             ground[location.x][location.y]=-1;
-
         }
-
     }
 
+    /**
+     * Moves all alive preys randomly.
+     */
     public void moveAllAlivePreysRandomly(){
         for (Prey prey : preyList){
             if(prey.isAlive)
@@ -41,7 +57,11 @@ public class Preys {
         }
     }
 
-
+    /**
+     * Kills the prey at the specified location.
+     * @param x The x-coordinate of the location.
+     * @param y The y-coordinate of the location.
+     */
     public void killPreyAt(int x, int y) {
         for(Prey prey:preyList){
             if(prey.location.x==x && prey.location.y==y){
